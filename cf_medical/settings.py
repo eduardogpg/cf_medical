cc11386e75d26f535b2d14ff58935caf9b5324da
Eduardo Ismael <eduardo78d@gmail.com>
2023-01-25 14:28:31 -0600

    MySQl connection

--- cf_medical/settings.py
+++ cf_medical/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from local_settings import PASSWORD_DB
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'cf_medical.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cf_medical',
+        'USER': 'root',
+        'PASSWORD': PASSWORD_DB,
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
