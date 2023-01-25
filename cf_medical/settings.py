b58a9f7415e73e58138f527a2b0408ccf806d830
Eduardo Ismael <eduardo78d@gmail.com>
2023-01-25 15:24:08 -0600

    Stati files

--- cf_medical/settings.py
+++ cf_medical/settings.py
@@ -123,8 +123,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = 'statifiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
