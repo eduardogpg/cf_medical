996891025ca3335442ca1856e56178e51c6ecac0
Eduardo Ismael <eduardo78d@gmail.com>
2023-01-27 11:38:17 -0600

    StaticFiles

--- cf_medical/settings.py
+++ cf_medical/settings.py
@@ -125,9 +125,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
-
+STATIC_ROOT = 'staticfiles'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
+    '45.55.40.115/static'
 ]
--- cf_medical/urls.py
+++ cf_medical/urls.py
@@ -17,7 +17,10 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('laboratories/', include('laboratories.urls')),
-] 
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
