c1efe43d39f36a10f2fb35b090097224b0b42aa4
Eduardo Ismael <eduardo78d@gmail.com>
2023-02-01 17:40:50 -0600

    H1 en laboratorios

--- fabfile.py
+++ fabfile.py
@@ -1,21 +1,24 @@
 from fabric.api import run
 from fabric.api import env, cd, prefix, sudo
 
-env.hosts =  ['104.236.4.54']
+env.hosts = ['104.236.4.54']
 env.user = 'eduardo'
 env.key_filename = '/home/eduardo/.ssh/id_ed25519.pub'
 
-
 def deploy():
+    print('>>> Nos contamos a nuestro servidor remoto.')
+
     with cd('project'):
         with cd('cf_medical'):
+            run('git pull')
+
             with prefix('source env/bin/activate'):
-                run('git pull')
-
-                # sudo('pip install -r requirements.txt')
+                run('pip install -r requirements.txt')
+                
                 run('python manage.py migrate')
+                run('python manage.py collectstatic --noinput')
+    
+    sudo('sudo systemctl restart django')
+    sudo('sudo systemctl restart nginx')
 
-                print('>>> Comenzamos con los servicios.')
-
-                sudo('sudo systemctl restart django')
-                sudo('sudo systemctl restart nginx')
+          
